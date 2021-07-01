c38ccf16ca64cc706bda2f872bf7c32c15e26114
EmilyLow <emlow@me.com>
2021-07-01 11:44:54 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import styled from 'styled-components';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GridDiv>
+     
+      <Contact>Contact Me</Contact>
+        <StyledHeader>
+        <h1>Emily Low</h1>
+        <h2>Full Stack Software Developer</h2>
+        </StyledHeader>
+       
+    </GridDiv>
   );
 }
 
 export default App;
+
+const GridDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1 fr 1fr 1fr;
+  grid-template-rows: 10% 100px 100px 80px auto;
+  //background-color: grey;
+`;
+
+const Contact = styled.p`
+  grid-column: 4 / span 1;
+  grid-row: 1 / span 1;
+
+`;
+
+const StyledHeader = styled.div`
+  grid-column: 2 / span 2;
+  grid-row: 2 / span 1;
+  //background-color: blue;
+`;
